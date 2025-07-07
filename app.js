777968e710c79fa5588e1f44a5388f4dc439fb50
aliahmadpersonal <aliahmadazhar4420@gmail.com>
2025-07-08 02:21:39 +0500

    .

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ const prisma = new PrismaClient();
 
 // Register plugins in correct order
 await fastify.register(fastifyCors, {
-  origin: ["http://localhost:3000", "http://127.0.0.1:3000"],
+  origin: ["http://localhost:3000", "http://127.0.0.1:3000","https://fasttrk.ai"],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization", "Accept"],
